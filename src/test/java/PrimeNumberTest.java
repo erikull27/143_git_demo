c4176c28aa7b60b301b93219d289d32df86ad434
erikull27 <erikullojka@gmail.com>
2024-01-08 12:31:59 -0800

    yay it passes the test!

--- src/test/java/PrimeNumberTest.java
+++ src/test/java/PrimeNumberTest.java
@@ -1,8 +1,19 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PrimeNumberTest {
     @Test
     public void myAwesomeTest() {
-        // TODO: add your test
+        //1. inputs
+        int input = 3;
+        //2. expected output
+        boolean expect = true;
+        //3. actual output
+        boolean actual = IsAPrimeNumber.isAPrimeNumber(input);
+        //4. is expected same as actual
+        assertEquals(expect, actual);
+
+
     }
 }
